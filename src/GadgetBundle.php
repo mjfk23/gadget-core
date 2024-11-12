2f944ee9e6fa5b01acff1d21607a0c7bc52f9138
Melody Kimball <melody@forestkimball.com>
2024-11-12 06:46:53 -0500

    Symfony bundle

--- src/GadgetBundle.php
+++ src/GadgetBundle.php
@@ -4,17 +4,8 @@ declare(strict_types=1);
 
 namespace Gadget;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 final class GadgetBundle extends Bundle
 {
-    /**
-     * @param ContainerBuilder $container
-     * @return void
-     */
-    public function build(ContainerBuilder $container): void
-    {
-        parent::build($container);
-    }
 }
