62a8505e63304c68ac5c5f43d2364998c052d63c
Melody Kimball <melody@forestkimball.com>
2024-12-20 11:11:55 -0500

    Updated Exception to use FormattedString

--- src/Exception/Exception.php
+++ src/Exception/Exception.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Gadget\Exception;
 
+use Gadget\Io\FormattedString;
+
 class Exception extends \Exception
 {
     /**
@@ -20,40 +22,9 @@ class Exception extends \Exception
         \Throwable|null $previous = null
     ) {
         parent::__construct(
-            $this->formatMessage(...(is_array($message) ? $message : [$message])),
+            strval(is_array($message) ? new FormattedString(...$message) : $message),
             $code,
             $previous
         );
-    }
-
-
-    /**
-     * @param string|\Stringable $message
-     * @param string|\Stringable|int|float|bool|null ...$values
-     * @return string
-     */
-    protected function formatMessage(
-        string|\Stringable $message,
-        string|\Stringable|int|float|bool|null ...$values
-    ): string {
-        return sprintf(
-            $message instanceof \Stringable ? $message->__toString() : $message,
-            ...array_map($this->formatMessageValue(...), $values)
-        );
-    }
-
-
-    /**
-     * @param string|\Stringable|int|float|bool|null $v
-     * @return string
-     */
-    protected function formatMessageValue(string|\Stringable|int|float|bool|null $v): string|int|float
-    {
-        return match (true) {
-            $v instanceof \Stringable => $v->__toString(),
-            $v === null => 'null',
-            is_bool($v) => $v ? 'true' : 'false',
-            default => $v
-        };
     }
 }
